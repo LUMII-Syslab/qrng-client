20913d1cf14f3617dca534d44f666e2ba4ebaca6
Sergejs Kozlovičs <sergejs.kozlovics@lumii.lv>
2023-10-30 12:35:19 +0200

    commented out debug messages

--- src/main/java/lv/lumii/smartcard/SmartCardClientTokenFactory.java
+++ src/main/java/lv/lumii/smartcard/SmartCardClientTokenFactory.java
@@ -1,6 +1,5 @@
 package lv.lumii.smartcard;
 
-import lv.lumii.pqc.InjectablePQC;
 import lv.lumii.qrng.ClientTokenFactory;
 import lv.lumii.qrng.QrngProperties;
 import lv.lumii.qrng.clienttoken.SmartCardToken;
@@ -22,6 +21,7 @@ public class SmartCardClientTokenFactory implements ClientTokenFactory {
         //   sudo service pcscd start
         // We also need to add this JVM option:
         //   -Dsun.security.smartcardio.library=/usr/lib/x86_64-linux-gnu/libpcsclite.so.1
+        // (or specify the smartcardLibrary property in qrng.properties)
 
         String smartCardReaderName = location;
         JavaCardCommunication comm = new JavaCardCommunication(smartCardReaderName);
@@ -29,20 +29,19 @@ public class SmartCardClientTokenFactory implements ClientTokenFactory {
         byte[] appledID = new byte[]{(byte) 0xa0, (byte) 0xb0, (byte) 0xc0, (byte) 0xd0, (byte) 0xe0};
         boolean b = comm.selectApplet(appledID);
         if (!b) throw new IllegalStateException("FAILED to create a communication with the card");
-        System.out.println("SUCCESSFULLY connected to the applet!");
+        //System.out.println("SUCCESSFULLY connected to the applet!");
 
-        System.out.println("~~~ Let's get the PUBLIC KEY! ~~~");
+        //System.out.println("~~~ Let's get the PUBLIC KEY! ~~~");
         byte[] pubKey = comm.getPublicKey();
 
-        System.out.println("~~~ Let's get the CERTIFICATE!  ~~~");
+        //System.out.println("~~~ Let's get the CERTIFICATE!  ~~~");
         byte[] cert = comm.getCertificate();
 
-        //SmartCardToken sct = new SmartCardToken(ft.certificateChain(), ft.key(),
-        SmartCardToken sct = new SmartCardToken(cert, //ft.key()/*dummy*/,
+        SmartCardToken sct = new SmartCardToken(cert,
                 (message) -> { // sign function
-                    System.out.println("CARD SIGNATURE");
+                    //System.out.println("CARD SIGNATURE");
                     byte[] signed = comm.sign(message);
-                    System.out.println("SIGNED: " + InjectablePQC.byteArrayToString(signed, " "));
+                    //System.out.println("SIGNED: " + InjectablePQC.byteArrayToString(signed, " "));
                     return signed;
                 });
         return sct;
